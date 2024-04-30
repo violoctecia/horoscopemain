d01f0865aa6ad1198e31d348050243e2a07fd999
violoctecia <violoctecia@gmail.com>
2024-04-30 18:20:57 +0300

    added dist

---
+++ src/pages/Main/MainPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainSection from "@/components/mainComponents/MainSection.vue";
+import ZodiacSection from "@/components/mainComponents/ZodiacSection.vue";
+import TelegramLink from "@/components/mainComponents/TelegramLink.vue";
+import EastSection from "@/components/mainComponents/EastSection.vue";
+import OtherSection from "@/components/mainComponents/OtherSection.vue";
+</script>
+
+<template>
+  <MainSection></MainSection>
+  <ZodiacSection></ZodiacSection>
+  <TelegramLink></TelegramLink>
+  <EastSection></EastSection>
+  <OtherSection></OtherSection>
+</template>
